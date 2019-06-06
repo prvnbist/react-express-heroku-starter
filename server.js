2fd2700262001da17879944391164a39a94f1c92
Praveen Bisht <prvnbist@gmail.com>
2019-06-06 15:06:01 +0530

    second attempt

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/data', (req, res) => res.send({data: 'Praveen Bisht'}))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port ${port}!`))
